d6489ceac312837f246bdba5aae1b7a8a646f65d
TankNut <m.vlasblom@live.nl>
2025-05-28 05:08:28 +0200

    Auto add workshop addons

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,1 +1,25 @@
 include("shared.lua")
+
+local whitelist = {
+	["materials"] = true,
+	["models"] = true,
+	["particles"] = true,
+	["resource"] = true,
+	["sound"] = true
+}
+
+for _, addon in ipairs(engine.GetAddons()) do
+	if not addon.mounted or not addon.downloaded then
+		continue
+	end
+
+	local _, folders = file.Find("*", addon.title)
+
+	for _, folder in ipairs(folders) do
+		if whitelist[folder] then
+			resource.AddWorkshop(addon.wsid)
+
+			break
+		end
+	end
+end
